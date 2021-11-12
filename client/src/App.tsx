e6f47d5faad5b1579fbed9cbe0d3c6c63b582cd3
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-11-13 00:47:01 +0200

    Add About page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App(): JSX.Element {
 	return (
 		<>
 			<Home />
+			<About/>
 		</>
 	);
 }
