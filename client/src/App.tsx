369ee490614efb6de65b33a4c49ce37aec76d80c
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-11-15 11:12:21 +0200

    Add app modal context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import { AppModalProvider } from "./utils/app-context";
 
 function App(): JSX.Element {
 	return (
-		<>
+		<AppModalProvider>
 			<Home />
-		</>
+		</AppModalProvider>
 	);
 }
 
--- client/src/layout/AddPost/AddPost.tsx
+++ client/src/layout/AddPost/AddPost.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ReactComponent as AddPostIcon } from './add-post.svg';
 import styles from './AddPost.module.scss';
+import { useAppModal } from "../../utils/app-context";
 
 const AddPost = (): JSX.Element => {
 
+	const { dispatch } = useAppModal();
+
 	return (
 		<div title="Create new post" className={styles.addPost}>
-			<button className={styles.addButton}>
+			<button className={styles.addButton} onClick={() => dispatch({ type: 'toggleModal' })}>
 				<AddPostIcon className={styles.addIcon} />
 			</button>
 		</div>
