e6e5b31fe5b3f8a8633f2df5c1db43bc2ee66c7a
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-11-30 00:56:04 +0200

    Remove unused mock data

--- client/src/utils/storage.ts
+++
@@ -1,9 +0,0 @@
-import { forEach } from "lodash";
-import { MockData } from "./interface";
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const initLocalStorage = (data: MockData) => {
-	forEach(data, (dataItem, idx) => {
-		localStorage.setItem(idx, JSON.stringify(dataItem));
-	});
-};
